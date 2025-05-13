cbbbf2b11c775e1e1d80e1bdb715d5a2cedd7a32
302A-01\cre <a>
2025-05-13 16:12:01 +0900

    try 使い方ミスってる

--- Kadai1.java
+++ Kadai1.java
@@ -1,19 +1,24 @@
 import java.util.Arrays;
 public class Kadai1 {
     public static void main (String[] args) {
-       int num = Integer.parseInt(args[0]);
-        int count = 0;
-        for (int i = 1; i <= 1000; i++) {
-            System.out.print(i);
-            count++;
-            if(count == num) {
-                count = 0;
-                System.out.println();
-            }else if(i == 1000){
-                break;
-            } else{
-                System.out.print(" ");
+        try {
+            int num = Integer.parseInt(args[0]);
+            catch(ArrayIndexOutOfBoundsException e){System.out.print("引数が指定されていません");}
+            catch (NumberFormatException e){System.out.print("引数が整数ではありません");}
+            int count = 0;
+            for (int i = 1; i <= 1000; i++) {
+                System.out.print(i);
+                count++;
+                if (count == num) {
+                    count = 0;
+                    System.out.println();
+                } else if (i == 1000) {
+                    break;
+                } else {
+                    System.out.print(" ");
+                }
             }
-        }System.out.print("");
+            System.out.print("");
+        }
     }
 }
