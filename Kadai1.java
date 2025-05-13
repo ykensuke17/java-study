814c27e73ac56344c2606077e0c7f7e9977846a0
302A-01\cre <a>
2025-05-13 15:42:19 +0900

    5/13 課題１完了

--- Kadai1.java
+++ Kadai1.java
@@ -1,13 +1,19 @@
+import java.util.Arrays;
 public class Kadai1 {
     public static void main (String[] args) {
+       int num = Integer.parseInt(args[0]);
         int count = 0;
         for (int i = 1; i <= 1000; i++) {
-            System.out.print(i + " ");
+            System.out.print(i);
             count++;
-            if(count == 10) {
+            if(count == num) {
                 count = 0;
                 System.out.println();
+            }else if(i == 1000){
+                break;
+            } else{
+                System.out.print(" ");
             }
-        }
+        }System.out.print("");
     }
 }
