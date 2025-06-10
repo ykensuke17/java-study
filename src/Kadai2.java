1f340a335661d6394c5982216d70b8a557e2fd73
302A-01\cre <a>
2025-06-10 15:13:57 +0900

    進捗同じ

--- src/Kadai2.java
+++ src/Kadai2.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class Kadai2 {
     public static void main(String[] args) {
+        try {
+            ArrayList<String> list = fileRead("BookInfo.csv");
+            for (String s: list) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         //        try (
 //                BufferedReader br = new BufferedReader(
 //                        new FileReader(
@@ -27,20 +35,18 @@ public class Kadai2 {
 //        }
     }
 
-    private static ArrayList<String> fileRead(String filePath) {
-        try (
-                BufferedReader br = new BufferedReader(
-                        new FileReader(
-                                "BookInfo.csv",
-                                Charset.forName("MS932")
-                        )
+    private static ArrayList<String> fileRead(String filePath) throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader(
+                        filePath,
+                        Charset.forName("MS932")
                 )
-        ) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] data = line.split(",");
-                System.out.println(line);
-            }
+        );
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] data = line.split(",");
+            System.out.println(line);
         }
+        return null;
     }
 }
