48a87b2cc632c6e24299cff108ff03760193ab22
302A-01\cre <a>
2025-06-10 14:23:19 +0900

    a

--- Kadai2.java
+++ Kadai2.java
@@ -4,21 +4,54 @@ import java.nio.file.Path;
 
 public class Kadai2 {
     public static void main(String[] args) {
-        try (
-                BufferedReader br = new BufferedReader(
-                        new FileReader(
-                                "BookInfo.csv",
-                                Charset.forName("MS932")
-                        )
-                )
-        ) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+
+
+//        try (
+//                BufferedReader br = new BufferedReader(
+//                        new FileReader(
+//                                "BookInfo.csv",
+//                                Charset.forName("MS932")
+//                        )
+//                )
+//        ) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                String[] data = line.split(",");
+//
+//                for (String elem : data) {
+//                    System.out.println("[" + elem + "]");
+//                }
+//
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
     }
+
+    private static ArrayList<String> fileRead(String filePath) {
+        new FileReader("BookInfo.csv")
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
