de069a942c6315cdc54a6a10e7255cc9eaaf0be0
302A-01\cre <a>
2025-06-24 15:03:17 +0900

    revert Kadai1

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -1,82 +1,26 @@
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-
-public class Kadai2 {
-    public static void main(String[] args) {
+public class Kadai1 {
+    public static void main (String[] args) {
+        int num = 0;
         try {
-            ArrayList<String> list = fileRead("BookInfo.csv");
-            for (String s: list) {
-                System.out.println(s);
+            num = Integer.parseInt(args[0]);
+            int count = 0;
+            for (int i = 1; i <= 1000; i++) {
+                System.out.print(i);
+                count++;
+                if (count == num) {
+                    count = 0;
+                    System.out.println();
+                } else if (i == 1000) {
+                    break;
+                } else {
+                    System.out.print(" ");
+                }
             }
-            ArrayList<Book> booklist = parseToBookList(list);
-
-
-
-
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }catch (NullPointerException e) {
-            System.out.println("以上です");
+            System.out.print("");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.print("引数が指定されていません");
+        } catch (NumberFormatException e) {
+            System.out.print("引数が整数ではありません");
         }
-        //        try (
-//                BufferedReader br = new BufferedReader(
-//                        new FileReader(
-//                                "BookInfo.csv",
-//                                Charset.forName("MS932")
-//                        )
-//                )
-//        ) {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                String[] data = line.split(",");
-//
-//                for (String elem : data) {
-//                    System.out.println("[" + elem + "]");
-//                }
-//
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    private static ArrayList<String> fileRead(String filePath) throws IOException {
-        BufferedReader br = new BufferedReader(
-                new FileReader(
-                        filePath,
-                        Charset.forName("MS932")
-                )
-        );
-
-        ArrayList<String> list = new ArrayList<>();
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] data = line.split(",");
-            list.add(line);
-        }
-        return list;
-    }
-
-    private static ArrayList<Book> parseToBookList(ArrayList<String> readLine){
-
-
-
-
-        return null;
-    }
-
-    private static void displyBookInfo(ArrayList<Book> bookList){
-
-
-
-
-
-
-
-        return;
     }
 }
