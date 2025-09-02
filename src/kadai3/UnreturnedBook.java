a4cbdd8a89c871495e77621932fe8628298ee42b
302A-01\cre <a>
2025-09-02 14:02:22 +0900

    課題3終了

--- src/kadai3/UnreturnedBook.java
+++ src/kadai3/UnreturnedBook.java
@@ -9,15 +9,17 @@ public class UnreturnedBook extends Book {
     /** 本の返却予定日 */
     public UnreturnedBook(String title, String author, Calendar loanDate, String price) {
         super(title, author, loanDate, price);
-        /**ここを直す↓*/
-        this.scheduledReturnDate = (Calendar) loanDate.clone();
+        this.scheduledReturnDate = Calendar.getInstance();
+        Date dloanDate = loanDate.getTime();
+        scheduledReturnDate.setTime(dloanDate);
         scheduledReturnDate.add(Calendar.DAY_OF_MONTH, 30);
+        System.out.println(this.loanDate);
+        System.out.println(this.scheduledReturnDate);
     }
 
     @Override
     public void displayBookInfo() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy年MM月dd日");
-        loanDate.add(Calendar.DAY_OF_MONTH, +30);
         System.out.println(
                 String.format("===========\nタイトル： %s\n著者： %s\n貸出日： %s\n返却日予定日： %s\n価格： %s円\n状態： 未返却\n===========",
                         this.title,
